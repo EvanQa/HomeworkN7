4c4b8a3bce7df751144a649e508a8e5a15fb5fe7
EvanQa <ivan.khri1488@gmail.com>
2024-09-22 18:14:45 +0300

    solution from teacher +

--- Exercise3.py
+++ Exercise3.py
@@ -1,3 +1,4 @@
+#my solution
 number: int = int(input('enter a num '))
 for row in range( 1 , number + 1):
     for col in range(1, row + 1):
@@ -11,3 +12,21 @@ for row in range(number - 1, 0, -1):
             continue
         print(" " ,col, end="")
     print()
+
+#teacher solution
+max_line: int = 0
+while max_line % 2 == 0:
+    max_line: int = int(input('max line'))
+
+for number in range(1, max_line + 2, 2):
+    spaces: int =  (max_line - number) // 2
+    print(' ' * spaces)
+    for i in range( 1, number + 1):
+        print(i, end="")
+
+
+for number in range(max_line - 2, 1 - 2, -2):
+    spaces: int =  (max_line - number) // 2
+    print(' ' * spaces)
+    for i in range(1, number + 1):
+        print(i, end="")
