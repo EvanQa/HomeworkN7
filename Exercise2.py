9b56dd97a2b3d5b5015190cda9c6a776ef1aa115
EvanQa <ivan.khri1488@gmail.com>
2024-09-22 18:04:59 +0300

    solution from teacher +

--- Exercise2.py
+++ Exercise2.py
@@ -1,3 +1,4 @@
+#my solution
 world_record = 6.23
 good_results_count = 0
 good_results_sum = 0
@@ -32,3 +33,33 @@ if record_holder:
     print(f"New world record: {world_record} meters by {record_holder}")
 else:
     print("None")
+
+#teacher solution
+WORLD_RECORD: float = 6.23
+new_world_record: float = None
+new_world_record_name: str = None
+count_good_result: int = 0
+sum_good_result: int = 0
+max_score: float = None
+MAX_COUNT: int = 7
+for _ in range(MAX_COUNT):
+    result: float = float(input("whats the height?"))
+    if result < 5.80:
+        continue
+    count_good_result += 1
+    sum_good_result = result
+    if not max_score is None or result > max_score:
+        max_score = result
+    if result > WORLD_RECORD:
+        new_world_record =result
+        new_world_record_name = input('whats the name of the jumper?')
+else:
+    avg_good_score: float = None
+    if count_good_result:
+        avg_good_score: float = sum_good_result / count_good_result
+    print(f"highest score: {max_score}")
+    if new_world_record:
+        print(f" new world record height: {new_world_record}")
+        print(f" new world record jumper name: {new_world_record_name}")
+    else:
+        print("no new record achieved. None")
